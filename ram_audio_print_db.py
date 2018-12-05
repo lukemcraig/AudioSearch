5b7af517ee71958ac23b178eb6b0e2625ee4a4be
Luke M Craig <lukemcduffiecraig@gmail.com>
2018-12-05 03:23:25 -0500

    loading both fingerprints and songs

--- ram_audio_print_db.py
+++ ram_audio_print_db.py
@@ -5,21 +5,41 @@ import sys
 
 class RamAudioPrintDB(AudioPrintsDB):
     def __init__(self):
+        fingerprints_hashtable = self.load_fingerprint_table()
+        songs_hashtable = self.load_song_table()
+        pass
+
+    def load_fingerprint_table(self):
         fingerprint_hashtable = {}
         with open('mongoexport/audioprintsDB.fingerprints.json', mode='r') as f:
             for line in f:
                 fingerprint = json.loads(line)
                 del fingerprint["_id"]
-                hash = fingerprint.pop("hash")
+                hash_ = fingerprint.pop("hash")
                 fingerprint_tuple = (fingerprint['offset'], fingerprint['songID'])
                 try:
-                    fingerprint_hashtable[hash].append(fingerprint_tuple)
+                    fingerprint_hashtable[hash_].append(fingerprint_tuple)
                 except KeyError:
-                    fingerprint_hashtable[hash] = [fingerprint_tuple]
+                    fingerprint_hashtable[hash_] = [fingerprint_tuple]
         print(len(fingerprint_hashtable), "unique fingerprint hashes")
         # this is not the actual size
         hashtable_size = sys.getsizeof(fingerprint_hashtable)
         print(hashtable_size, "bytes")
+        return fingerprint_hashtable
+
+    def load_song_table(self):
+        songs_hashtable = {}
+        with open('mongoexport/audioprintsDB.songs.json', mode='r') as f:
+            for line in f:
+                song = json.loads(line)
+                song_id = song.pop("_id")
+                song_tuple = (song['artist'], song['album'], song['title'], song['track_length_s'])
+                try:
+                    songs_hashtable[song_id].append(song_tuple)
+                except KeyError:
+                    songs_hashtable[song_id] = [song_tuple]
+        print(len(songs_hashtable), "songs")
+        return songs_hashtable
 
     def insert_one_fingerprint(self, fingerprint):
         return
