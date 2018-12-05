e11721661680f19346e65bedf0af9b86b452e2ca
Luke M Craig <lukemcduffiecraig@gmail.com>
2018-12-05 03:40:19 -0500

    implemented find one song

--- ram_audio_print_db.py
+++ ram_audio_print_db.py
@@ -5,8 +5,8 @@ import sys
 
 class RamAudioPrintDB(AudioPrintsDB):
     def __init__(self):
-        fingerprints_hashtable = self.load_fingerprint_table()
-        songs_hashtable = self.load_song_table()
+        self.fingerprints_hashtable = self.load_fingerprint_table()
+        self.songs_hashtable, self.songtitles_hashtable = self.load_song_table()
         pass
 
     def load_fingerprint_table(self):
@@ -29,23 +29,33 @@ class RamAudioPrintDB(AudioPrintsDB):
 
     def load_song_table(self):
         songs_hashtable = {}
+        songtitles_hashtable = {}
         with open('mongoexport/audioprintsDB.songs.json', mode='r') as f:
             for line in f:
                 song = json.loads(line)
                 song_id = song.pop("_id")
                 song_tuple = (song['artist'], song['album'], song['title'], song['track_length_s'])
                 try:
-                    songs_hashtable[song_id].append(song_tuple)
+                    songtitles_hashtable[song['title']].append(song_id)
                 except KeyError:
-                    songs_hashtable[song_id] = [song_tuple]
+                    songtitles_hashtable[song['title']] = [song_id]
+                songs_hashtable[song_id] = song_tuple
         print(len(songs_hashtable), "songs")
-        return songs_hashtable
+        return songs_hashtable, songtitles_hashtable
 
     def insert_one_fingerprint(self, fingerprint):
         return
 
     def find_one_song(self, song):
-        return
+        matching_titles = self.songtitles_hashtable[song['title']]
+        for possible_song_id in matching_titles:
+            possible_song = self.songs_hashtable[possible_song_id]
+            if song['artist'] == possible_song[0]:
+                if song['album'] == possible_song[1]:
+                    if song['track_length_s'] == possible_song[3]:
+                        song_doc = {"_id": 0}
+                        return song_doc
+        return None
 
     def get_next_song_id(self):
         return
